3dae3997c46e418bb5ef8952bd1ddb440e6c50e8
Khalil Snell <khalil.snell@gmail.com>
2025-03-13 13:37:55 -0400

    wheel of fortune 1.2

--- app/wheel-of-fortune/components/gameBoard.tsx
+++ app/wheel-of-fortune/components/gameBoard.tsx
@@ -15,7 +15,7 @@ export default function GameBoard() {
     const [solveError, setSovleError] = useState("");
     const [vowelError, setVowelError] = useState("");
     const [playerArray, setPlayerArray] = useState([
-        {name: "Khalil", score: 0, color: "red", player: "player"}, 
+        {name: "", score: 0, color: "red", player: "player"}, 
         {name: "Annie", score: 0, color: "blue", player: "cpu"}, 
         {name: "Frank", score: 0, color: "yellow", player: "cpu"}]);
     const [currentSpin, setCurrentSpin] = useState("");
@@ -31,12 +31,13 @@ export default function GameBoard() {
     useEffect(() => { 
         if (!gameInitialized) return;
 
-        if (checkIfPlayerCPU(currentPlayer)) {
+        if (checkIfPlayerCPU(currentPlayer) && !checkAnswerComplete()) {
             //console.log("currentSpin: " + currentSpin);
             if (currentSpin != "" && currentSpin != "lost turn" && currentSpin != "bankrupt") {
                 runCPUPlayer(); 
             } else {
                 triggerCPUPlayer(currentPlayer);
+                return;
             }
             
             if (currentSpin == "") {
@@ -45,20 +46,31 @@ export default function GameBoard() {
         }
     }, [currentSpin, currentPlayer]);
 
-    if (typeof document !== "undefined") {
-        wheel = document.getElementById("wheel");
-        playerName = (document.getElementById("playerName") as HTMLInputElement);
-        guessInput = (document.getElementById("guess") as HTMLInputElement);
-        solveInput = (document.getElementById("solve") as HTMLInputElement);
-        vowelInput = (document.getElementById("vowel") as HTMLInputElement);
-    } else return;
+    //if (!setHTMLElementVariables()) return;
+
+    function setHTMLElementVariables() {
+        let result = false;
+        
+        if (typeof document !== "undefined") {
+            result = true;
+            wheel = document.getElementById("wheel");
+            playerName = (document.getElementById("playerName") as HTMLInputElement);
+            guessInput = (document.getElementById("guess") as HTMLInputElement);
+            solveInput = (document.getElementById("solve") as HTMLInputElement);
+            vowelInput = (document.getElementById("vowel") as HTMLInputElement);
+        }
+
+        return result;
+    }
 
     function handleGameInitialization() {
-        setGameInitialized(true);
+        setHTMLElementVariables();
 
         if (playerName) {
             playerArray[0].name = playerName.value;
         }
+        
+        setGameInitialized(true);
 
         if (answer == "") {
             const randomAnswer = Math.floor(Math.random() * answerList.length);
@@ -74,14 +86,16 @@ export default function GameBoard() {
         setIsSolved(false);
         setGuessedLetters([]);
         setPlayerArray([
-            {name: "Khalil", score: 0, color: "red", player: "player"}, 
+            {name: "", score: 0, color: "red", player: "player"}, 
             {name: "Annie", score: 0, color: "blue", player: "cpu"}, 
             {name: "Frank", score: 0, color: "yellow", player: "cpu"}
         ]);
         setGuessError("");
         setSovleError("");
         setVowelError("");
-        
+
+        setCurrentPlayer(0);
+        handlePlayerTurn("", 0);
 
         const randomAnswer = Math.floor(Math.random() * answerList.length);
         setAnswer(answerList[randomAnswer].value);
@@ -153,6 +167,10 @@ export default function GameBoard() {
     }
 
     function solvePuzzle() {
+        if (!solveInput) {
+            setHTMLElementVariables();
+        }
+
         const puzzle = solveInput.value;
         const result = checkSolvePuzzle(puzzle.toUpperCase());
         
@@ -236,10 +254,6 @@ export default function GameBoard() {
             setGuessedLetters([...guessedLetters, mValue]);
             incrementCurrentPlayer();
             handlePlayerTurn("", getNextPlayer());
-            //console.log(checkIfPlayerCPU(getNextPlayer()));
-            /*if (checkIfPlayerCPU(getNextPlayer())) {
-                setTimeout(() => { triggerCPUPlayer(getNextPlayer()) }, 5000);
-            }*/
         } else {
             //console.log(playerArray);
 
@@ -258,13 +272,18 @@ export default function GameBoard() {
     } 
 
     function buildGameBoard() {
-        if (!document) return;
-
         const gbArray = [];
         let output = <></>;
         const wordArray = answer.split(" ");
-        let widthLeft = document.body.clientWidth;
+        let widthLeft = 0;
         let j = 0;
+        let singleCharLength = 55;
+
+        if (typeof document !== "undefined") {
+            widthLeft = document.body.clientWidth;
+
+            if (widthLeft <= 600) singleCharLength = 25;
+        }
 
         if (!gameInitialized) {
             output = (
@@ -286,20 +305,20 @@ export default function GameBoard() {
             return output;
         } else {
             for (let i = 0; i < answer.length; i++) {
-                let cName = "m-2 w-12 text-center inline-block text-4xl ";
+                let cName = "m-1 md:m-2 w-5 md:w-12 text-center inline-block text-lg md:text-4xl ";
                 const letter = answer[i];
 
                 if (!isSolved && (!guessedLetters.includes(letter) && letter.match(/[A-Z ]/))) cName += "text-white ";
-                if (letter != " ") cName += "bg-white border-black border-4";
+                if (letter != " ") cName += "bg-white border-black border-2 md:border-4";
                 else { j++; }
 
-                widthLeft = widthLeft - 55;
+                widthLeft = widthLeft - singleCharLength;
 
                 gbArray.push(
                     <div key={widthLeft + "_" + letter + "_" + i} className={cName}>{letter}</div>
                 );
                 
-                if (letter == " " && widthLeft - ((wordArray[j].length + 1) * 55) < 55) {
+                if (letter == " " && widthLeft - ((wordArray[j].length + 1) * singleCharLength) < singleCharLength) {
                     gbArray.push(<br key={widthLeft + "_br_" + i} />);
                     widthLeft = document.body.clientWidth;
                 }
@@ -315,6 +334,9 @@ export default function GameBoard() {
         if (!gameInitialized) return;
 
         if (checkAnswerComplete() || isSolved) {
+            //setIsSolved(true);
+            //setGameInitialized(false);
+
             playerBoard = (
                 <>
                 <span className="text-green-600">Congratulations! {playerArray[currentPlayer].name} guessed the puzzle!</span>  
@@ -381,7 +403,7 @@ export default function GameBoard() {
         const spin = 15 * Math.floor(Math.random() * 24);
         let spinValue: string = "";
 
-        //spin = 165;
+        //if (currentPlayer == 1) spin = 105;
 
         setTimeout(() => { 
             resetPlayerBoard(); 
@@ -462,16 +484,16 @@ export default function GameBoard() {
         return (
             <>
             <div className="inline-block text-center border-red-500 border-4 pl-5 pr-5 bg-red-300 rounded-xl">
-                <div className="pl-5 pr-5">{playerArray[0].name}</div>
-                <div className="pl-5 pr-5">{playerArray[0].score}</div>
+                <div className="pl-2 pr-2 md:pl-5 md:pr-5">{playerArray[0].name}</div>
+                <div className="pl-2 pr-2 md:pl-5 md:pr-5">{playerArray[0].score}</div>
             </div>
             <div className="inline-block text-center border-blue-500 border-4 pl-5 pr-5 bg-blue-300 rounded-xl">
-                <div className="pl-5 pr-5">{playerArray[1].name}</div>
-                <div className="pl-5 pr-5">{playerArray[1].score}</div>
+                <div className="pl-2 pr-2 md:pl-5 md:pr-5">{playerArray[1].name}</div>
+                <div className="pl-2 pr-2 md:pl-5 md:pr-5">{playerArray[1].score}</div>
             </div>
             <div className="inline-block text-center border-yellow-500 border-4 pr-5 pl-5 bg-yellow-300 rounded-xl">
-                <div className="pl-5 pr-5">{playerArray[2].name}</div>
-                <div className="pl-5 pr-5">{playerArray[2].score}</div>
+                <div className="pl-2 pr-2 md:pl-5 md:pr-5">{playerArray[2].name}</div>
+                <div className="pl-2 pr-2 md:pl-5 md:pr-5">{playerArray[2].score}</div>
             </div>
             </>
         );
@@ -488,12 +510,12 @@ export default function GameBoard() {
 
         if (spinValue === "") {
             if (wContainer && pBoard) {
-                wContainer.className = "mt-5 border border-black rounded-xl bg-" + color + "-300";
+                wContainer.className = "relative mt-5 border border-black rounded-xl bg-" + color + "-300";
                 pBoard.className = "p-5 border border-black rounded-xl bg-white";
             }
         } else {
             if (wContainer && pBoard) {
-                wContainer.className = "mt-5 border border-black rounded-xl bg-white";
+                wContainer.className = "relative mt-5 border border-black rounded-xl bg-white";
                 pBoard.className = "p-5 border border-black rounded-xl bg-" + color + "-300";
             }
         }
@@ -501,7 +523,7 @@ export default function GameBoard() {
 
     return (
     <>
-        <div className="mt-32 border-black border-3 bg-blue-600 rounded-xl text-center text-white">{clue}</div>
+        <div className="mt-5 md:mt-32 border-black border-3 bg-blue-600 rounded-xl text-center text-white">{clue}</div>
         <div 
             id="gameBoard"
             className="text-left bg-emerald-600 w-full h-60 rounded-xl"
@@ -514,12 +536,13 @@ export default function GameBoard() {
         {getPlayerBoard()}
         <div 
             id="wheelContainer" 
-            className="mt-5 border border-black rounded-xl bg-red-300">
+            className="relative mt-5 border border-black rounded-xl bg-red-300">
             <button 
                 id="spin"
                 onClick={spinWheel}
                 className="border border-black p-2 ml-3 rounded-lg"
                 >SPIN!!!</button>
+            <div className='arrow-down'></div>
             <img
                 id="wheel"
                 src="./WOFWheel.svg"
